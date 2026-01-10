794703806a5b1e6e93b648513b9c35d588a55dc0
officialij2005@gmail.com <officialij2005@gmail.com>
2026-01-10 16:52:11 +0800

    just added footer

---
+++ appointmentSQL/src/Footer.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; Backend practice project. Created by IJ2005.</p>
+      {/* Yes i know, the frontEnd is vibe coded haha! This is just a practice for my backend - sql. If you're reading this, hello! */}
+    </footer>
+  )
+}
+
+export default Footer
